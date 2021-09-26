35fba3a60e8b040e41ea47dd9a783e374b0b0760
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2021-09-26 09:20:09 -0400

    Fixed some bugs in selection function

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,11 @@ from pyrogram import Client, emoji, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.handlers import CallbackQueryHandler
 from types import SimpleNamespace
+from button import ButtonFactory
 
 import random, asyncio
 
+URL_REGEX_PATTERN = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)'
 
 async def selection(app: Client,
                     user: int,
@@ -20,11 +22,11 @@ async def selection(app: Client,
     ns.done = False
     ns.canceled = False
 
-    total_pages = len(options) / max_options_per_page \
-                    + (1 if len(options) % max_options_per_page == 0 else 0)
+    total_pages = len(options) // max_options_per_page \
+                    + (1 if len(options) % max_options_per_page != 0 else 0)
 
     prefix = random.randbytes(20)
-    rid = {prefix + random.randbytes(44): k for k in options}
+    rid = {prefix + random.randbytes(44): k for k in options}    
 
     next_id = random.randbytes(64)
     back_id = random.randbytes(64)
@@ -43,13 +45,17 @@ async def selection(app: Client,
         if len(name) >= 20:
             name = name[:20]
         selected = (name in opt)
-        return InlineKeyboardButton(
-            f"{emoji.CHECK_MARK_BUTTON if selected else ''}{name}",
-            callback_data=id)
+        return [
+            InlineKeyboardButton(
+                f"{emoji.CHECK_MARK_BUTTON if selected else ''}{name}",
+                callback_data=id)
+        ]
 
     def navigation_buttons():
-        back = InlineKeyboardButton(emoji.LEFT_ARROW, callback_data=back_id)
-        next = InlineKeyboardButton(emoji.RIGHT_ARROW, callback_data=next_id)
+        back = InlineKeyboardButton(f'{emoji.LEFT_ARROW} {ns.page - 1}',
+                                    callback_data=back_id)
+        next = InlineKeyboardButton(f'{ns.page + 1} {emoji.RIGHT_ARROW}',
+                                    callback_data=next_id)
         ret = []
         if ns.page > 0:
             ret.append(back)
@@ -58,7 +64,7 @@ async def selection(app: Client,
 
         return ret
 
-    items = rid.items()
+    items = list(rid.items())
 
     async def _select(app: Client, callback_query: CallbackQuery):
         a = ns.page * max_options_per_page
@@ -66,13 +72,16 @@ async def selection(app: Client,
         page_items = items[a:b]
 
         # yapf: disable
-        done_button = [InlineKeyboardButton('Done', callback_data=done_id)]
-        cancel_button = [InlineKeyboardButton('Cancel', callback_data=cancel_id)]
+        done_button = InlineKeyboardButton('Done', callback_data=done_id)
+        cancel_button = InlineKeyboardButton('Cancel', callback_data=cancel_id)
 
         extra = [
-            [InlineKeyboardButton('Select All', callback_data=select_all_id)],
-            [InlineKeyboardButton('Unselect All', callback_data=unselect_all_id)]
-        ] + [done_button, cancel_button]
+            [
+                InlineKeyboardButton('Select All', callback_data=select_all_id),
+                InlineKeyboardButton('Unselect All', callback_data=unselect_all_id)
+            ],
+            [done_button, cancel_button]
+        ]
 
         markup = InlineKeyboardMarkup(
             [create_button(opt) for opt in page_items] +
@@ -103,17 +112,17 @@ async def selection(app: Client,
             ns.page -= 1
         await _select(app, callback_query)
 
-    async def _done(app: Client, callback_query: CallbackQuery):
-        callback_query.message.delete(True)
+    async def _done(_, callback_query: CallbackQuery):
+        await callback_query.message.delete(True)
         ns.done = True
 
-    async def _cancel(app: Client, callback_query: CallbackQuery):
+    async def _cancel(_, callback_query: CallbackQuery):
+        await callback_query.message.delete(True)
         ns.canceled = True
         ns.done = True
 
     async def _select_item(app: Client, callback_query: CallbackQuery):
-        data = callback_query.data[20:]
-        m = rid[data]
+        m = rid[callback_query.data]
 
         if m in opt:
             opt.pop(m)
@@ -126,22 +135,31 @@ async def selection(app: Client,
             callback_query.message.delete(True)
             ns.done = True
 
-    app.add_handler(
-        CallbackQueryHandler(_select_item, filters=starts_with(prefix)))
-
-    app.add_handler(
-        CallbackQueryHandler(_next_page, filters=exact_match(next_id)))
-    app.add_handler(
-        CallbackQueryHandler(_back_page, filters=exact_match(back_id)))
-    app.add_handler(
-        CallbackQueryHandler(_select_all, filters=exact_match(select_all_id)))
-    app.add_handler(
-        CallbackQueryHandler(_unselect_all,
-                             filters=exact_match(unselect_all_id)))
+    ret = [
+        app.add_handler(
+            CallbackQueryHandler(_select_item, filters=starts_with(prefix))),
+        app.add_handler(
+            CallbackQueryHandler(_next_page, filters=exact_match(next_id))),
+        app.add_handler(
+            CallbackQueryHandler(_back_page, filters=exact_match(back_id))),
+        app.add_handler(
+            CallbackQueryHandler(_select_all,
+                                 filters=exact_match(select_all_id))),
+        app.add_handler(
+            CallbackQueryHandler(_unselect_all,
+                                 filters=exact_match(unselect_all_id))),
+        app.add_handler(
+            CallbackQueryHandler(_done, filters=exact_match(done_id))),
+        app.add_handler(
+            CallbackQueryHandler(_cancel, filters=exact_match(cancel_id))),
+    ]
 
     await _select(app, None)
 
     while not ns.done:
-        await asyncio.sleep(500)
+        await asyncio.sleep(0.5)
+
+    for h, g in ret:
+        app.remove_handler(h, g)
 
     return opt if not ns.canceled else None
